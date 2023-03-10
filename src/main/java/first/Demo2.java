a210d549f23645a520ab066ab3ab10a651f3e70b
Harish Kailasa <hari.kailasa@gmail.com>
2023-03-11 04:45:03 +0530

    My Second commit

---
+++ src/main/java/first/Demo2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package first;
+
+/**
+ * @author Harish
+ *
+ */
+public class Demo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
